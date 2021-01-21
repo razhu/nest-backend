edd8fe302a224b66fb790d48930a495e5a0383bc
ramiro <rhuayller@agetic.gob.bo>
2021-01-21 15:54:35 -0400

    ok

---
+++ src/users/users.entity.ts
@@ -0,0 +1,14 @@
+import {Entity, PrimaryGeneratedColumn, Column} from 'typeorm'
+
+
+@Entity('users')
+export class UsersEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    username: string;
+
+    @Column()
+    email: string
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+    // constructor goes here
+    constructor(){}
+    // methods go here
+    findAll() {
+        return [{}, {}]
+    }
+}
